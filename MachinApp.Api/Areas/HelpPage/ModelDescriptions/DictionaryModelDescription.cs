3964b548e205d52c50904ddf67d384b957ee53e3
MAURICIO RINCON ARCILA <mauriciorincon30158@correo.itm.edu.co>
2019-01-09 20:49:20 -0500

    Project creation

---
+++ MachinApp.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MachinApp.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MachinApp.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MachinApp.Api.WebApiApplication" Language="C#" %>
